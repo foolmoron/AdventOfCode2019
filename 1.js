6017f9f46d7860d12fb71d9138f1603d9f8597d5
Momin Khan <foolmoron@gmail.com>
2019-12-01 17:28:49 -0500

    1

--- 1.js
+++ 1.js
@@ -1,1 +1,19 @@
-console.log('test')
+// *
+input.map(z=>Math.floor(z/3)-2).reduce((a, i) => a + i, 0)
+
+// **
+function calcFuel(m) {
+    let total = 0;
+    while (true) {
+        let fuel = Math.floor(m/3)-2;
+        if (fuel > 0) {
+            total += fuel;
+        }
+        m = fuel;
+        if (m <= 0) {
+            return total;
+        }
+    }
+}
+
+input.map(z=>calcFuel(z)).reduce((a, i) => a + i, 0)
